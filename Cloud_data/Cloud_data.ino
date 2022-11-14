cd8022ef11225e96fa2d422733e384595fd2fcac
DuyKobe <113445128+DuyKobe@users.noreply.github.com>
2022-11-14 21:51:09 +0700

    Update Cloud_data

--- Cloud_data/Cloud_data.ino
+++ Cloud_data/Cloud_data.ino
@@ -1,10 +1,48 @@
 // Libraries
+OBDII-UART-Serial version 9
+* This program will talk to vehicle using the OBDII-UART board, 
+* and display the results on the LCD, and upload to freeboard IoT platform
+* 
+* Author: zhaoshentech
+* Updated: 2018-08-27
+* 
+* updates:
+*   v3: modified the getResponse() function so that the buffer receives the correct response.
+*       add the getRPM() to get the engine RPM from the vehicle.
+*   v4: add the getSpeed() function to get the speed of the vehicle
+*   v5: add the LCD module and display the speed and RPM on the LCD
+*   v6: is the wifi version
+*   v7: is the non-wifi, non-serial version. Remove serial initialization,
+*       so that the board can work without a computer.
+*   v8: is the non-wifi, non-serial version. Add fuel level and coolant temperature.
+*       rearrange the display location.
+*   v9: is the wifi, non-serial version. Upolad speed, RPM, fuel level and coolant temperture
+* 
+* LCD circuit connection:
+* LCD RS pin to digitial pin 12
+* LCD Enable pin to digital pin 11
+* LCD D4 pin to digital pin 5
+* LCD D5 pin to digital pin 4
+* LCD D6 pin to digital pin 3 
+* LCD D7 pin to digital pin 2
+* LCD R/W pin to ground
+* 10 K potentialmeter:
+* ends to +5V and ground
+* wiper to LCD VO pin (pin 3)
+*/
+
+////////////////////////////////////////////////////////
+//
+// WiFi related //
+///////////////////////////////////////////////////////
+// ket noi board voi wifi
 #include <SPI.h>
 #include <WiFi101.h>
 
 // Credentials
-char ssid[] = "Bao Minh Coffee";     //  your network SSID (name)
-char pass[] = "baominhcoffee";  // your network password
+char ssid[] = "Bao Minh Coffee";     //  your network SSID (name)// wifi ID  tạo store network 
+char pass[] = "baominhcoffee";  // your network password,tao pass store network 
+char server[] = "https://dweet.io";  // freeboard and dweet Settings
 int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
 void setup() {
